db4faabfb0ded619c8e0c0b6c32735df73702099
Cristian Buitrago <Crisis1882@gmail.com>
2021-03-04 15:03:53 -0500

    Navbar y header terminados

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,13 +1,24 @@
 const button = document.getElementById('navButton')
 const navBar = document.getElementById('nav')
+const main = document.getElementById('main')
 
-button.addEventListener('focus', showNav)
-button.addEventListener('focusout', hideNav)
-
-function showNav() {
-  navBar.classList.add('show')
-}
-
-function hideNav()  {
+const removeShow = () => {
   navBar.classList.remove('show')
+  counter = 1
+}
+const addShow = () => navBar.classList.add('show')
+
+main.addEventListener('click', removeShow)
+
+let counter = 1
+
+button.onclick = () => {
+  console.log(counter)
+
+  if(counter === 1) {
+    addShow()
+    counter = 0
+  } else if (counter === 0) {
+    removeShow()
+  }
 }
