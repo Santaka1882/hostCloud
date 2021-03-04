a05110692885357c170d075d8a111f8d5ea9a718
Cristian Buitrago <Crisis1882@gmail.com>
2021-03-04 15:44:06 -0500

    Cambios al los estilos del header y navbar

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,19 +2,21 @@ const button = document.getElementById('navButton')
 const navBar = document.getElementById('nav')
 const main = document.getElementById('main')
 
+let counter = 1
+
 const removeShow = () => {
   navBar.classList.remove('show')
+  main.classList.remove('dark')
   counter = 1
 }
-const addShow = () => navBar.classList.add('show')
+const addShow = () => {
+  navBar.classList.add('show')
+  main.classList.add('dark')
+}
 
 main.addEventListener('click', removeShow)
 
-let counter = 1
-
 button.onclick = () => {
-  console.log(counter)
-
   if(counter === 1) {
     addShow()
     counter = 0
